601d09a3bf616217115dad5475f62b66d1604d34
Brice Huang <bmhuang@mit.edu>
2019-10-30 02:06:12 -0700

    difficulty 4096

--- contracts/DarkForestV1Core.sol
+++ contracts/DarkForestV1Core.sol
@@ -10,10 +10,10 @@ contract DarkForestV1 is Verifier {
 
     uint public xSize = 1024;
     uint public ySize = 1024;
-    uint public difficulty = 16384;
+    uint public difficulty = 4096;
     uint capacity = 100000; // in milliPopulation
     uint growth = 100; // maximum growth rate, achieved at milliPops = 50000, in milliPopulation per second
-    uint moveDecayNumerator = 160;
+    uint moveDecayNumerator = 80;
 
     uint256 constant LOCATION_ID_UB = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
