f870425566f04a4c58f9cb3418c5590210a021d5
Matthew Huang <matthewhuang@Matthews-MBP.attlocal.net>
2019-12-03 05:16:10 -0800

    [Client] Refactor Landing + Loading scenes

---
+++ client/src/app/LoadingPage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const LoadingPage = () => {
+  return (
+    <div className="h-full w-full flex items-center justify-center">
+      Loading... (this may take a few seconds)
+    </div>
+  );
+};
+
+export default LoadingPage;
