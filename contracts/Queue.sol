0c742e1b205b3fbd2b82fe409b4fff5fb47d98ab
Robert Cunningham <robertvcunningham@gmail.com>
2020-02-12 17:49:15 -0500

    Add Queue

--- contracts/Queue.sol
+++ contracts/Queue.sol
@@ -3,31 +3,7 @@
 contract queue {
     struct Queue {
         uint[] data;
-        uint front;
-        uint back;
     }
 
-    function length(Queue storage q) internal view returns (uint) {
-        return q.back - q.front;
-    }
 
-    function capacity(Queue storage q) internal view returns (uint) {
-        return q.data.length - 1;
-    }
-
-    function push(Queue storage q, uint data) internal {
-        require((q.back + 1) % q.data.length != q.front);
-
-        q.data[q.back] = data;
-        q.back = (q.back + 1) % q.data.length;
-    }
-
-    function pop(Queue storage q) internal returns (uint) {
-        require(q.back != q.front);
-
-        uint out = q.data[q.front];
-        delete q.data[q.front];
-        q.front = (q.front + 1) % q.data.length;
-        return out;
-    }
 }
