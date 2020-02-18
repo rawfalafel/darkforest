c26525d37bc038a7c5966644fcd5b2c22fa63871
Alan Luo <me@alanluo.com>
2020-02-18 00:11:22 -0500

    Update WindowManager.tsx

--- client/src/app/WindowManager.tsx
+++ client/src/app/WindowManager.tsx
@@ -5,6 +5,9 @@ import Viewport from './board/Viewport';
 import { CanvasCoords, WorldCoords } from '../utils/Coordinates';
 import GameManager from '../api/GameManager';
 import UIEmitter from '../utils/UIEmitter'
+import { Planet } from '../@types/global/global';
+
+import { getCurrentPopulation } from '../utils/Utils';
 
 interface WindowProps {}
 interface WindowState {}
@@ -12,21 +15,21 @@ interface WindowState {}
 class WindowManager extends React.Component<WindowProps, WindowState> {
 
 	state = {
-		forces : 0,
+		forces : 25,
 		activeTab: 'details',
-		planet : {
-			capacity: 0,
-			growth: 0,
-			hardiness: 0,
-			stalwartness: 0,
-		}
+		planet : null,
 	};
 	uiManager = GameUIManager.getInstance();
 	gameManager = GameManager.getInstance();
 
 	updateInfo() {
-		console.log("planet has been selected!")
-		this.setState({planet: this.uiManager.selectedPlanet});
+		if(this.uiManager.selectedPlanet != null) {
+			this.setState({planet: this.uiManager.selectedPlanet});
+		}
+	}
+	animate() {
+		this.updateInfo();
+		window.requestAnimationFrame(this.animate.bind(this));
 	}
 	constructor(props: WindowProps) {
 		super(props);
@@ -46,15 +49,33 @@ class WindowManager extends React.Component<WindowProps, WindowState> {
 		}
 
 		uiEmitter.on('GAME_PLANET_SELECTED', this.updateInfo.bind(this));
+
+		window.requestAnimationFrame(this.animate.bind(this));
 	}
 	handleForcesChange = (e) => {
 		this.setState({forces: e.target.value});
+	}
+	renderPlanetProp(prop : string):string {
+		if(prop == "population") {
+			return (this.state.planet
+					? (Math.round(getCurrentPopulation(this.state.planet)/100.0)).toString() 
+					: "0");
+		} else if(prop == "scaledPopulation") {
+			return (this.state.planet
+					? ((this.state.forces/100.0)*Math.round(getCurrentPopulation(this.state.planet)/100.0)).toString() 
+					: "0");
+
+		} else { 
+			return (this.state.planet 
+					? (Math.round(this.state.planet[prop]/100.0)).toString() 
+					: "0");
+		}
 	}
 	render() {
 		return (
 			<div className="flex flex-col
                       bg-gray-900 border border-white m-2 rounded-sm" 
-                      style={{width: "18rem",  height:"12rem"}}>
+                      style={{width: "18rem",  height:"14rem"}}>
 	          {/* Tabs */}
 	          <div className="flex flex-row justify-between m-2">
 	            <a onClick={() => this.setState({activeTab: 'details'})}
@@ -71,20 +92,24 @@ class WindowManager extends React.Component<WindowProps, WindowState> {
 	              <table className="width-full" style={{width:"100%"}}>
 	              	<tbody className="width-full" style={{width:"100%"}}>
 		              <tr>
+		              	<td colSpan={2}>Population:</td>
+		              	<td colSpan={2} className="text-right">{this.renderPlanetProp("population")}</td>
+		              </tr>
+		              <tr>
 		              	<td colSpan={2}>Capacity:</td>
-		              	<td colSpan={2} className="text-right">{(Math.round(this.state.planet.capacity/100.0)).toString()}</td>
+		              	<td colSpan={2} className="text-right">{this.renderPlanetProp("capacity")}</td>
 		              </tr>
 		              <tr>
 		              	<td colSpan={2}>Growth:</td>
-		              	<td colSpan={2} className="text-right">{(Math.round(this.state.planet.growth/100.0)).toString()}</td>
+		              	<td colSpan={2} className="text-right">{this.renderPlanetProp("growth")}</td>
 		              </tr>
 		              <tr>
 		              	<td colSpan={2}>Hardiness:</td>
-		              	<td colSpan={2} className="text-right">{(Math.round(this.state.planet.hardiness/100.0)).toString()}</td>
+		              	<td colSpan={2} className="text-right">{this.renderPlanetProp("hardiness")}</td>
 		              </tr>
 		              <tr>
 		              	<td colSpan={2}>Stalwartness:</td>
-		              	<td colSpan={2} className="text-right">{(Math.round(this.state.planet.stalwartness/100.0)).toString()}</td>
+		              	<td colSpan={2} className="text-right">{this.renderPlanetProp("stalwartness")}</td>
 		              </tr>
 		              <tr><td colSpan={4}></td></tr>
 		              <tr>
@@ -106,20 +131,28 @@ class WindowManager extends React.Component<WindowProps, WindowState> {
 	            </div>
 
 	            <div className={this.state.activeTab == 'forces' ? "block" : "hidden"}>
-	              <p>Forces to send: 
-	              <select value={this.state.forces}
-
-	              	className="bg-gray-700 border border-white p-2 rounded-none" 
-	              	onChange={this.handleForcesChange}>
-	                <option value="10">10%</option>
-	                <option value="25">25%</option>
-	                <option value="50">50%</option>
-	                <option value="75">75%</option>
-	                <option value="100">100%</option>
-	              </select>
-	              </p>
-	              {/* <p>{this.state.forces}% of current planet: 
-	              	{this.state.planet ? (this.state.forces/100)*this.state.planet.capacity : ""}</p> */}
+	            <table>
+	            	<tbody>
+	            		<tr>
+	            			<td>Forces to send:</td>
+	            			<td>
+	            			  <select value={this.state.forces}
+				              	className="bg-gray-700 border border-white p-2 rounded-none" 
+				              	onChange={this.handleForcesChange}>
+				                <option value="10">10%</option>
+				                <option value="25">25%</option>
+				                <option value="50">50%</option>
+				                <option value="75">75%</option>
+				                <option value="100">100%</option>
+				              </select>
+	              			</td>
+	            		</tr>
+	            		<tr>
+	            			<td colSpan={2}>{this.state.forces}% of current planet:
+	            			&nbsp;<b>{this.renderPlanetProp("scaledPopulation")}</b></td>
+	            		</tr>
+	            	</tbody>
+	            </table>
 	            </div>
 	          </div>
 
